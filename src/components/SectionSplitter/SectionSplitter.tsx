40a7a370a0a2b300fa7b40ccba69afc95a7a412a
Rafy01 <150427662+Rafy01@users.noreply.github.com>
2025-05-22 19:42:57 +0200

    feat: add SectionSplitter component with styling

---
+++ src/components/SectionSplitter/SectionSplitter.tsx
@@ -0,0 +1,8 @@
+// components/SectionSplitter/SectionSplitter.tsx
+
+import React from "react";
+import "./SectionSplitter.css";
+
+export function SectionSplitter() {
+  return <div className="section-splitter" aria-hidden="true" />;
+}
