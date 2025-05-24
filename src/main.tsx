f385934d457d6b7018e16157d66c5be59a75e26a
Rafy01 <150427662+Rafy01@users.noreply.github.com>
2025-05-25 00:52:10 +0200

    added klaviyo

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,4 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import Originality from "./Pages/Originality/Originality";
-// import "./index.css";
-
-// ReactDOM.createRoot(document.getElementById("root")!).render(
-//   <React.StrictMode>
-//     <Originality />
-//   </React.StrictMode>
-// );
+// main.tsx eller main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -17,6 +8,17 @@ import Originality from "./Pages/Originality/Originality";
 import BlogPost from "./Pages/Blog/BlogPost";
 import "./index.css";
 import NotFound from "./Pages/NotFound/NotFound";
+
+// Inject Klaviyo script
+(function loadKlaviyoScript() {
+  const script = document.createElement("script");
+  script.src =
+    "https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=R6MMxy";
+  script.async = true;
+  document.head.appendChild(script);
+})();
+
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
