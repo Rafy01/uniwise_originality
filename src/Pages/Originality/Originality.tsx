ad2f28f3c71e1ed360891543fa85c3eaaefd1906
Rafy01 <150427662+Rafy01@users.noreply.github.com>
2025-05-27 00:48:01 +0200

    #

--- src/Pages/Originality/Originality.tsx
+++ src/Pages/Originality/Originality.tsx
@@ -33,10 +33,10 @@ export default function OriginalityPage() {
           property="og:description"
           content="AI-drevet plagieringstjek indlejret i WISEflow."
         />
-        <meta
+        {/* <meta
           property="og:image"
           content="{`${baseUrl}/UNIwise_logo_white.png"
-        />
+        /> */}
         {/* <meta property="og:url" content={`${baseUrl}/originality`} /> */}
         <meta property="og:type" content="website" />
         <meta property="og:image:width" content="1200" />
